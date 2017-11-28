a2d86a4738d51a3bc75fc0e83f73c6b8e6bd7463
kmalek <katarzyna.m1405@gmail.com>
2017-11-28 15:59:02 +0100

    first commit

---
+++ src/API/exampleAPI.js
@@ -0,0 +1,15 @@
+// export default function getNewsApi(page){
+//     return new Promise((resolve, reject)=>{
+//         document.querySelector('button.button.more-news').setAttribute('disabled', 'disabled');
+//         resolve(
+//             fetch(`https://hn.algolia.com/api/v1/search?query=tags=story&hitsPerPage=20&page=${page}&format=json`)
+//             .then((response)=>{
+//              return response.text()
+//             })
+//             .then(function(stories){
+//                 if(JSON.parse(stories).hits.length != 0) {
+//                     document.querySelector('button.button.more-news').removeAttribute('disabled');
+//                }
+//                 return stories ? JSON.parse(stories):{}; 
+//             }))
+//     })}
